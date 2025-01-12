da3f9913c2b4743bc2c07c3cfc732d954ae4815b
VKTRenokh <viktorenokh@gmail.com>
2025-01-12 22:45:27 +0200

    feat: implement blimpy

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,47 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn enableRawMode(fd: std.posix.fd_t) !void {
+    const state = try std.posix.tcgetattr(fd);
+    var raw = state;
+    raw.iflag.IGNBRK = false;
+    raw.iflag.BRKINT = false;
+    raw.iflag.PARMRK = false;
+    raw.iflag.ISTRIP = false;
+    raw.iflag.INLCR = false;
+    raw.iflag.IGNCR = false;
+    raw.iflag.ICRNL = false;
+    raw.iflag.IXON = false;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    raw.oflag.OPOST = false;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    raw.lflag.ECHO = false;
+    raw.lflag.ECHONL = false;
+    raw.lflag.ICANON = false;
+    raw.lflag.ISIG = false;
+    raw.lflag.IEXTEN = false;
 
-    try bw.flush(); // don't forget to flush!
+    raw.cflag.CSIZE = .CS8;
+    raw.cflag.PARENB = false;
+
+    raw.cc[@intFromEnum(std.posix.V.MIN)] = 1;
+    raw.cc[@intFromEnum(std.posix.V.TIME)] = 0;
+
+    try std.posix.tcsetattr(fd, .FLUSH, raw);
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    const tty = try std.posix.open("/dev/tty", .{ .ACCMODE = .RDWR }, 0);
+
+    try enableRawMode(tty);
+
+    var buffer: [100]u8 = undefined;
+
+    const stdin = std.io.getStdIn();
+    const stdout = std.io.getStdOut();
+
+    while (true) {
+        _ = try stdin.read(&buffer);
+
+        try stdout.writeAll("blimpy ");
+    }
 }
